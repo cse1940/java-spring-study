121a556ceeaa0b2ab4e742d6dbe55d9b9ad85651
cse1940 <se1940@naver.com>
2022-11-14 11:09:55 +0900

    221114_modify

---
+++ Chapter2/src/ch17/Company.java
@@ -0,0 +1,14 @@
+package ch17;
+
+public class Company {
+	private static Company instance = new Company(); // 유일한 객체가 됨
+	
+	private Company() {}
+	
+	public static Company getInstance() {
+		if(instance == null) {
+			instance = new Company();
+		}
+		return instance;
+	}
+}
---
+++ Chapter2/src/ch17/CompanyTest.java
@@ -0,0 +1,17 @@
+package ch17;
+
+import java.util.Calendar;
+
+public class CompanyTest {
+
+	public static void main(String[] args) {
+		Company c1 = Company.getInstance();
+		Company c2 = Company.getInstance();
+		
+		System.out.println(c1);
+		System.out.println(c2);
+		
+		Calendar calendar = Calendar.getInstance();
+	}
+
+}
