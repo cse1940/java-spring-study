049c6b5deabcc6f8080f4a6409705a7071918a8f
cse1940 <se1940@naver.com>
2022-11-14 17:03:45 +0900

    221114_upload

---
+++ Chapter3/src/ch02/VIPCustomer.java
@@ -0,0 +1,16 @@
+package ch02;
+
+public class VIPCustomer extends Customer {
+	private int agentID;
+	double salesRatio;
+	
+	public VIPCustomer() {
+		bonusRatio = 0.05;
+		salesRatio = 0.1;
+		customerGrade = "VIP";
+	}
+	
+	public int getAgentID() {
+		return agentID;
+	}
+}
