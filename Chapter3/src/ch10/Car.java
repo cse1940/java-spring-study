f594d4d2dc087eb15d0cc7bfa3fe65a3fc0faf90
cse1940 <se1940@naver.com>
2022-11-15 11:58:32 +0900

    221115_upload

---
+++ Chapter3/src/ch10/Car.java
@@ -0,0 +1,24 @@
+package ch10;
+
+public abstract class Car {
+	public abstract void drive();
+	public abstract void stop();
+	
+	public void startCar() {
+		System.out.println("시동을 켭니다.");
+	}
+	
+	public void turnOff() {
+		System.out.println("시동을 끕니다.");
+	}
+	
+	public void washCar() {}
+	
+	final public void run() { // 오버라이딩(재정의) 불가
+		startCar();
+		drive();
+		stop();
+		turnOff();
+		washCar();
+	}
+}
