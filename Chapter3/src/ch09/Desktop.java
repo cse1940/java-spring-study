0873e32559a086a2762e7c23797d93d28d2e84df
cse1940 <se1940@naver.com>
2022-11-16 10:48:59 +0900

    221116_upload

---
+++ Chapter3/src/ch09/Desktop.java
@@ -0,0 +1,21 @@
+package ch09;
+
+public class Desktop extends Computer{
+
+	@Override
+	void display() {
+		System.out.println("Desktop display");
+	}
+
+	@Override
+	void typing() {
+		System.out.println("Desktop typing");
+	}
+
+	@Override
+	public void turnOff() {
+		System.out.println("Desktop turnOff");
+	}
+	
+	
+}
