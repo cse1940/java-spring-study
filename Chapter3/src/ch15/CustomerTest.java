20d5ab4ec7004283623fa3dcaab4520dc7570699
cse1940 <se1940@naver.com>
2022-11-17 16:11:28 +0900

    221117_upload

---
+++ Chapter3/src/ch15/CustomerTest.java
@@ -0,0 +1,25 @@
+package ch15;
+
+public class CustomerTest {
+
+	public static void main(String[] args) {
+		Customer customer = new Customer();
+		customer.buy();
+		customer.sell();
+		customer.order();
+		customer.hello();
+		
+		System.out.println("=======================");
+		
+		Buy buyer = customer;
+		buyer.buy();
+		buyer.order();
+		
+		System.out.println("=======================");
+		
+		Sell seller = customer;
+		seller.order();
+		seller.sell();
+	}
+
+}
